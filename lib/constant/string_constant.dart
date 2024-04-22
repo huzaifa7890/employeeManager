dd276f33e8ad22949b3dfcde0a909bad06460bde
Huzaifa Abubakr <huzaifa@appstree.io>
2024-04-22 15:11:55 +0500

    jfdsl

---
+++ lib/constant/string_constant.dart
@@ -0,0 +1,45 @@
+class AssetImages {
+  static const String appLogo = 'assets/images/app_logo.png';
+  static const String personIcon = 'assets/icons/person.png';
+}
+
+class AppRoutes {
+  static const String mainScreen = "/";
+  static const String loginScreen = "/login";
+  static const String splashScreen = "/splash";
+  static const String registerProfileScreen = "/register-profile";
+  static const String registerAccountScreen = "/register-account";
+  static const String forgotPasswordScreen = "/forgot-password";
+  static const String homeScreen = "/home";
+  static const String otpEnterScreen = "/enter-otp";
+  static const String otpVerifyScreen = "/verify-otp";
+  static const String navigationScreen = "/dashboard";
+  static const String buyerSellerScreen = "/select-buyer-seller";
+  static const String questionsSellerScerrn = "/seller-questions";
+  static const String verificationBuyerScreen = "/buyer-verification";
+  static const String questionsBuyerScreen = "/buyer-questions";
+  static const String searchBuyerScreen = "/search-for-Buyer";
+  static const String sellerNavigationScreen = "/seller-navigation";
+  static const String buyerNavigationScreen = "/buyer-navigation";
+  static const String addPropertyScreen = "/add-Property";
+  static const String buyerPropertiesScreen = "/buyer-Properties-Screen";
+  static const String sellerPropertiesScreen = "/seller-Properties-Screen";
+  static const String sellerPropertyDetailScreen = "/seller-property-detail";
+  static const String buyerpropertyDetailScreen = "/buyer-property-detail";
+  static const String allSellerOffersScreen = "/all-seller-offer-screen";
+  static const String chatScreen = "/chat";
+  static const String chatDetailScreen = "/chat-detail";
+  static const String pickLocationScreen = "/pick-location";
+  static const String propertyImageDetailScreen =
+      "/property-imageDetail-screen";
+  static const String onboarding = '/on-boarding';
+  static const String selectPropertyLocationScreen = "/pick-property-location";
+  static const String selectUserLocationScreen = "/pick-User-location";
+  static const String buyerCallScreen = "/buyer-Call-Screen";
+  static const String buyerEditProfileScreen = "/buyer-Edit-Profile-Screen";
+  static const String offersScreen = "/offers-screen";
+  static const String buyerFilterScreen = "/buyer-Filter-Screen";
+  static const String phoneVerificationScreen = "/phone-verification-screen";
+  static const String emailVerificationScreen = "/email-verification-screen";
+  static const String videoCaputure = "/video-capture";
+}
