3a41eefe160a29709e53e9d445e2f8dff2371250
Huzaifa Abubakr <huzaifa@appstree.io>
2024-04-24 18:19:29 +0500

    fixes

--- lib/feature/employee/add_employee/screen/add_employee_screen.dart
+++ lib/feature/employee/add_employee/screen/add_employee_screen.dart
@@ -1,5 +1,13 @@
+import 'package:employeemanager/constant/string_constant.dart';
+import 'package:employeemanager/core/service/image_picker_service.dart';
+import 'package:employeemanager/feature/auth/providers/image_provider.dart';
+import 'package:employeemanager/feature/auth/providers/user_provider.dart';
+import 'package:employeemanager/feature/auth/screens/register_profile_screen.dart';
+import 'package:employeemanager/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddEmployeeScreen extends ConsumerStatefulWidget {
   const AddEmployeeScreen({super.key});
@@ -20,6 +28,8 @@ class _AddEmployeeScreenState extends ConsumerState<AddEmployeeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final userProfilePic = ref.watch(userProvider)?.profilePic;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Employee'),
@@ -32,19 +42,237 @@ class _AddEmployeeScreenState extends ConsumerState<AddEmployeeScreen> {
             children: [
               Row(
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      // Handle image picker
-                    },
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundColor: Colors.grey[200],
-                      child: Icon(
-                        Icons.camera_alt,
-                        size: 40,
-                        color: Colors.grey[800],
+                  Stack(
+                    children: [
+                      CircleAvatar(
+                          maxRadius: 60,
+                          minRadius: 60,
+                          backgroundColor:
+                              AppColors.fieldGrey, // Placeholder color
+                          child: (userProfilePic != null)
+                              ? ClipRRect(
+                                  borderRadius: BorderRadius.circular(100),
+                                  child: Image.network(
+                                    "userProfilePic,",
+                                    height: 70,
+                                    width: 100,
+                                    fit: BoxFit.cover,
+                                  ),
+                                )
+                              : null
+                          // : (pickedImage != null)
+                          //     ? ClipRRect(
+                          //         borderRadius: BorderRadius.circular(100),
+                          //         child: Image.file(
+                          //           pickedImage,
+                          //           height: 100,
+                          //           width: 100,
+                          //           scale: 1,
+                          //           filterQuality: FilterQuality.medium,
+                          //           fit: BoxFit.cover,
+                          //         ),
+                          //       )
+                          //     : Image.asset(
+                          //         AssetImages.personIcon,
+                          //         height: 55,
+                          //         width: 55,
+                          //         fit: BoxFit.contain,
+                          //       ),
+                          ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: AppColors.darkBlue),
+                          child: Center(
+                            child: IconButton(
+                              icon: const Icon(Icons.edit),
+                              iconSize: 16,
+                              color: AppColors.primary,
+                              onPressed: () {
+                                ref.read(overlayProvider.notifier).state = true;
+                              },
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+
+                      /////// click me
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: AppColors.darkBlue),
+                          child: Center(
+                            child: IconButton(
+                              icon: const Icon(Icons.edit),
+                              iconSize: 16,
+                              color: AppColors.primary,
+                              onPressed: () async {
+                                showModalBottomSheet<void>(
+                                    context: context,
+                                    isScrollControlled: true,
+                                    backgroundColor: Colors.transparent,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(25)),
+                                    builder: (BuildContext context) {
+                                      return SizedBox(
+                                        width: double.infinity,
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: [
+                                            GestureDetector(
+                                              onTap: () async {
+                                                final imagePickerService =
+                                                    ImagePickerService();
+                                                final mediaUrls =
+                                                    await imagePickerService
+                                                        .imagePicker(
+                                                            ImageSource.camera);
+                                                if (mediaUrls != null) {
+                                                  ref
+                                                      .read(imagePickerProvider
+                                                          .notifier)
+                                                      .state = mediaUrls;
+                                                  context.pop();
+                                                }
+                                              },
+                                              child: Container(
+                                                height: 50,
+                                                width: 327,
+                                                decoration: BoxDecoration(
+                                                  color: AppColors.primary,
+                                                  borderRadius:
+                                                      BorderRadius.circular(25),
+                                                ),
+                                                child: Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  children: [
+                                                    const Icon(
+                                                      Icons.camera_alt_outlined,
+                                                      color: AppColors
+                                                          .textMessageBtnColor,
+                                                    ),
+                                                    const SizedBox(width: 15),
+                                                    Text(
+                                                      "Take Photo",
+                                                      style: theme
+                                                          .textTheme.bodyMedium
+                                                          ?.copyWith(
+                                                              color: AppColors
+                                                                  .textMessageBtnColor),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 20),
+                                            GestureDetector(
+                                              onTap: () async {
+                                                final imagePickerService =
+                                                    ImagePickerService();
+                                                final mediaUrls =
+                                                    await imagePickerService
+                                                        .imagePicker(ImageSource
+                                                            .gallery);
+
+                                                ref
+                                                    .read(imagePickerProvider
+                                                        .notifier)
+                                                    .state = mediaUrls;
+                                                context.pop();
+                                              },
+                                              child: Container(
+                                                height: 50,
+                                                width: 327,
+                                                decoration: BoxDecoration(
+                                                  color: AppColors.primary,
+                                                  borderRadius:
+                                                      BorderRadius.circular(25),
+                                                ),
+                                                child: Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  children: [
+                                                    const Icon(
+                                                      Icons.image_outlined,
+                                                      color: AppColors
+                                                          .textMessageBtnColor,
+                                                    ),
+                                                    const SizedBox(width: 15),
+                                                    Text(
+                                                      "Upload From Gallery",
+                                                      style: theme
+                                                          .textTheme.bodyMedium
+                                                          ?.copyWith(
+                                                        color: AppColors
+                                                            .textMessageBtnColor,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 20),
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  bottom: 40),
+                                              child: GestureDetector(
+                                                onTap: () {
+                                                  context.pop();
+                                                },
+                                                child: Container(
+                                                  height: 50,
+                                                  width: 327,
+                                                  decoration: BoxDecoration(
+                                                    color: AppColors.primary,
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            25),
+                                                  ),
+                                                  child: Row(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .center,
+                                                    children: [
+                                                      const SizedBox(width: 15),
+                                                      Text(
+                                                        "Cancel",
+                                                        style: theme.textTheme
+                                                            .bodyMedium
+                                                            ?.copyWith(
+                                                          color: AppColors
+                                                              .textMessageBtnColor,
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      );
+                                    });
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   const SizedBox(width: 20),
                   Expanded(
