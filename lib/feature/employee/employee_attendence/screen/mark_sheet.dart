dd1c05fa483f16c4ed027c6fc40f373c99dbd7f1
Huzaifa Abubakr <huzaifa@appstree.io>
2024-05-15 22:29:59 +0500

    fixes

--- lib/feature/employee/employee_attendence/screen/mark_sheet.dart
+++ lib/feature/employee/employee_attendence/screen/mark_sheet.dart
@@ -4,6 +4,7 @@ import 'package:employeemanager/feature/employee/employee_attendence/provider/at
 import 'package:employeemanager/feature/employee/employee_attendence/widgets/attendence_widget.dart';
 import 'package:employeemanager/models/employee.dart';
 import 'package:employeemanager/theme/app_colors.dart';
+import 'package:employeemanager/widgets/app_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
@@ -27,142 +28,233 @@ class _MarkAttendenceSheetState extends ConsumerState<MarkAttendenceSheet> {
       appBar: AppBar(
         title: const Text("Mark Attendence"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Column(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                color: AppColors.fieldGrey,
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Column(
-                        children: [
-                          Container(
-                            height: 80,
-                            width: 80,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(60),
-                              border: Border.all(
-                                color: Colors.blue,
-                                width:
-                                    4, // Adjust this value to your preference
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20),
+          child: Column(
+            children: [
+              Container(
+                height: 90,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: AppColors.fieldGrey,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                              height: 80,
+                              width: 80,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(60),
+                                border: Border.all(
+                                  color: Colors.blue,
+                                  width:
+                                      4, // Adjust this value to your preference
+                                ),
+                              ),
+                              child: CircleAvatar(
+                                maxRadius: 60,
+                                minRadius: 60,
+                                backgroundColor: AppColors.fieldGrey,
+                                child: widget.employee.employeePic!.isNotEmpty
+                                    ? ClipRRect(
+                                        borderRadius: BorderRadius.circular(51),
+                                        child: Image.network(
+                                          widget.employee.employeePic!,
+                                          height: 100,
+                                          width: 100,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      )
+                                    : Image.asset(
+                                        AssetImages.personIcon,
+                                        height: 55,
+                                        width: 55,
+                                        fit: BoxFit.contain,
+                                      ),
                               ),
                             ),
-                            child: CircleAvatar(
-                              maxRadius: 60,
-                              minRadius: 60,
-                              backgroundColor: AppColors.fieldGrey,
-                              child: widget.employee.employeePic!.isNotEmpty
-                                  ? ClipRRect(
-                                      borderRadius: BorderRadius.circular(51),
-                                      child: Image.network(
-                                        widget.employee.employeePic!,
-                                        height: 100,
-                                        width: 100,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    )
-                                  : Image.asset(
-                                      AssetImages.personIcon,
-                                      height: 55,
-                                      width: 55,
-                                      fit: BoxFit.contain,
-                                    ),
+                          ],
+                        ),
+                        const SizedBox(width: 20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.employee.name!,
+                              style: theme.textTheme.bodyLarge,
                             ),
-                          ),
-                        ],
+                            Text(
+                              (widget.employee.designation).toString(),
+                              style: theme.textTheme.bodyMedium,
+                            ),
+                            Text(
+                              (widget.employee.cnicId).toString(),
+                              style: theme.textTheme.bodyMedium,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "Date",
+                    style: theme.textTheme.bodyLarge,
+                  ),
+                  Container(
+                    width: 200,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: AppColors.purple,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        DateFormat('dd MMMM y').format(DateTime.now()),
+                        style: theme.textTheme.bodyLarge!
+                            .copyWith(color: AppColors.primary),
                       ),
-                      const SizedBox(width: 20),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.employee.name!,
-                            style: theme.textTheme.bodyLarge,
-                          ),
-                          Text(
-                            (widget.employee.designation).toString(),
-                            style: theme.textTheme.bodyMedium,
-                          ),
-                          Text(
-                            (widget.employee.cnicId).toString(),
-                            style: theme.textTheme.bodyMedium,
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
                   ),
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  "Date",
-                  style: theme.textTheme.bodyLarge,
-                ),
-                Container(
-                  width: 200,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: AppColors.purple,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      DateFormat('dd MMMM y').format(DateTime.now()),
-                      style: theme.textTheme.bodyLarge!
-                          .copyWith(color: AppColors.primary),
-                    ),
+              const SizedBox(height: 20),
+              // Display attendance checkboxes
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Center(
+                  child: Wrap(
+                    spacing: 8,
+                    children: EmployeeAttendenceStatus.values.map((item) {
+                      return AttendenceMarkWidget(
+                        text: getEmployeeAttendenceStatus(item),
+                        isSelected: selectEmployeeAttendence == item,
+                        onPressed: () {
+                          ref
+                              .read(attendenceProvider.notifier)
+                              .setAttendence(item);
+                        },
+                      );
+                    }).toList(),
                   ),
                 ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            // Display attendance checkboxes
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                // Text(
-                //   "Attendance",
-                //   style: theme.textTheme.bodyLarge,
-                // ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Center(
-                    child: Wrap(
-                      spacing: 8,
-                      children: EmployeeAttendenceStatus.values.map((item) {
-                        return AttendenceMarkWidget(
-                          text: getEmployeeAttendenceStatus(item),
-                          isSelected: selectEmployeeAttendence == item,
-                          onPressed: () {
-                            ref
-                                .read(attendenceProvider.notifier)
-                                .setAttendence(item);
-                          },
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                ),
+              ),
+              const SizedBox(height: 10),
+              AppTextField(
+                height: 65,
+                enabled: false,
+                hintText: "Basic Pay",
 
-                const SizedBox(height: 20),
-                // ElevatedButton(
-                //   onPressed: () {
-                //     // Store the attendance data
-                //     print(attendanceMap);
-                //   },
-                //   child: Text("Submit"),
-                // ),
-              ],
-            ),
-          ],
+                textAlign: TextAlign.end,
+                initialValue: widget.employee.pay.toString(),
+                // textController: cnicController,
+                fillColor: AppColors.fieldGrey,
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please Check Basic Pay";
+                  }
+                  return null;
+                },
+
+                hintStyle: theme.textTheme.bodyLarge,
+                lines: 1,
+              ),
+              const SizedBox(height: 10),
+              AppTextField(
+                height: 65,
+                // enabled: false,
+                hintText: "Tax/Debit",
+
+                textAlign: TextAlign.end,
+                // initialValue: widget.employee.pay.toString(),
+                // textController: cnicController,
+                fillColor: AppColors.fieldGrey,
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please Check Tax/Debit";
+                  }
+                  return null;
+                },
+
+                hintStyle: theme.textTheme.bodyLarge,
+                lines: 1,
+              ),
+              const SizedBox(height: 10),
+              AppTextField(
+                height: 65,
+                // enabled: false,
+                hintText: "Bonus",
+
+                textAlign: TextAlign.end,
+                // initialValue: widget.employee.pay.toString(),
+                // textController: cnicController,
+                fillColor: AppColors.fieldGrey,
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please Check Bonus";
+                  }
+                  return null;
+                },
+
+                hintStyle: theme.textTheme.bodyLarge,
+                lines: 1,
+              ),
+              const SizedBox(height: 10),
+              AppTextField(
+                height: 65,
+                // enabled: false,
+                hintText: "Total Payments",
+
+                textAlign: TextAlign.end,
+                // initialValue: widget.employee.pay.toString(),
+                // textController: cnicController,
+                fillColor: AppColors.fieldGrey,
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please Check Total Payments";
+                  }
+                  return null;
+                },
+
+                hintStyle: theme.textTheme.bodyLarge,
+                lines: 1,
+              ),
+            ],
+          ),
         ),
       ),
     );
